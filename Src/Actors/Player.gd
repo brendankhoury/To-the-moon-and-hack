f153907981bedbbd20dcdd4ab12b9f02aecef31a
Nicholas Chite <nickman736@gmail.com>
2020-07-31 14:28:32 -0400

    Added input for tap and update rotation print

--- Src/Actors/Player.gd
+++ Src/Actors/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export var rotation_speed := 0.5
+export var rotation_speed := deg2rad(1)
 
 # https://godotengine.org/qa/30772/how-to-create-a-grappling-hook-2d
 # https://docs.godotengine.org/en/stable/tutorials/physics/ray-casting.html
@@ -8,7 +8,7 @@ export var rotation_speed := 0.5
 func grapple(rotation: float):
 	var space_state = get_world_2d().direct_space_state
 	var result = space_state.intersect_ray(Vector2(0, 0), Vector2(50, 100))
-	print("Grapple function called", rotation)
+	print("Grapple function called, rotation: ", int(rad2deg(rotation))%360);
 
 func _physics_process(delta: float) -> void:
 	

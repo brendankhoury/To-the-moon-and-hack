4cc32698f2a427f784117a50b8f4bbed9ff29e86
Nicholas Chite <nickman736@gmail.com>
2020-07-31 14:18:44 -0400

    Clean up and changed asteroid to kinematic body

--- Src/Actors/asteroid.gd
+++ Src/Actors/asteroid.gd
@@ -1,6 +1,4 @@
-extends Area2D
-
-
+extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,16 +8,4 @@ func _ready():
 	shape.points.append(Vector2(1,0))
 	shape.points.append(Vector2(1,1))
 	shape.points.append(Vector2(0,1))
-	
-	
-	
-	
-	
-	
-	
-	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass

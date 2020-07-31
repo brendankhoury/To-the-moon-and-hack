f515dbbfbfb4f4adfd5853b65156ad55682293b8
Nicholas Chite <nickman736@gmail.com>
2020-07-31 16:52:18 -0400

    Asteroids fall on angles

--- Src/Actors/Asteroid.gd
+++ Src/Actors/Asteroid.gd
@@ -2,12 +2,18 @@ extends KinematicBody2D
 
 export var rotation_speed := deg2rad(1)
 export var move_speed := 1;
+export var max_angle := deg2rad(15)
+
+var angle := deg2rad(90)
+var vector := Vector2.DOWN
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	angle += rand_range(-max_angle, max_angle)
+	vector = Vector2(cos(angle), sin(angle))
 
 func _physics_process(delta: float) -> void:
-	move_and_collide(Vector2.DOWN)
+	
+	move_and_collide(vector)
 	rotate(rotation_speed)
 

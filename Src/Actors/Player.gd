9faf8f88b90bf7648b6e1f6ad18665123ff05117
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-07-31 10:50:16 -0700

    Made player rotate and added grapple detection

--- Src/Actors/Player.gd
+++ Src/Actors/Player.gd
@@ -1,6 +1,20 @@
 extends KinematicBody2D
 
+export var rotation_speed := 0.5
+
+# https://godotengine.org/qa/30772/how-to-create-a-grappling-hook-2d
+# https://docs.godotengine.org/en/stable/tutorials/physics/ray-casting.html
+
+func grapple(rotation: float):
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(Vector2(0, 0), Vector2(50, 100))
+	print("Grapple function called", rotation)
 
 func _physics_process(delta: float) -> void:
+	
+	#Temporary Rotation:
+	
+	rotate(rotation_speed)
 	if Input.is_action_just_pressed("tap"):
-		print("Tap detected")
+		grapple(rotation)
+

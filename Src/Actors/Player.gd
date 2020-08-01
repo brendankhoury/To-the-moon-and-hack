7e3066a0223b941eac67a5cf3b18b70d2ada4044
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-07-31 17:12:55 -0700

    Added stuck

--- Src/Actors/Player.gd
+++ Src/Actors/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+
 export var rotation_speed := deg2rad(0)
 export var grapple_speed: float = 350
 export var max_grapple = 500
@@ -8,7 +9,11 @@ var is_grappling: bool = false
 var grapple_length: float = 30.0
 var grapple_dir: Vector2 = Vector2.DOWN
 var grapple_collided: bool = false
+
 var collision_point: Vector2 = Vector2(0, 0)
+var stuck: bool= true
+
+var asteroid : Node2D
 
 var velocity := Vector2.ZERO
 # https://godotengine.org/qa/30772/how-to-create-a-grappling-hook-2d
@@ -19,13 +24,16 @@ func grapple(length: float, angle: float):
 	raycast.set_cast_to(Vector2(0, length))
 	grapple_collided = raycast.is_colliding()
 	if grapple_collided:
-		if abs(global_position.distance_to(raycast.get_collision_point())) < 150:
-			collision_point = raycast.get_collision_point()	
-		else: 
-			grapple_length = 30
-			grapple_collided = false
-			is_grappling = false
-	
+#		if abs(global_position.distance_to(raycast.get_collision_point())) < 150:
+		asteroid = raycast.get_collider()
+		print("C")
+		
+		collision_point = asteroid.to_local(raycast.get_collision_point())
+#		else: 
+#			grapple_length = 30
+#			grapple_collided = false
+#			is_grappling = false
+
 func _draw() -> void:
 	draw_line(
 		Vector2(0, 0), 
@@ -55,17 +63,31 @@ func _physics_process(delta: float) -> void:
 			#consider dissabling the raycast for performance purposes
 			is_grappling = false
 	elif grapple_collided:
-		grapple_length = raycast.global_position.distance_to(collision_point)
-		grapple_dir = raycast.global_position.direction_to(collision_point)
+		
+		
+		var global_position_point = asteroid.to_global(collision_point)
+		print(global_position_point)
+		grapple_length = raycast.global_position.distance_to(global_position_point)
+		grapple_dir = raycast.global_position.direction_to(global_position_point)
+		
 #		var grapple_angle:float = raycast.position.angle_to_point(to_local(collision_point))
 		position += grapple_speed * grapple_dir * delta
+#		move_and_slide(grapple_dir)
 		grapple_length -= grapple_speed * delta
 	elif grapple_length > 0.0:
 		grapple_length -= grapple_speed * delta
 		if grapple_length < 0:
 			grapple_length = 0
+	elif stuck:
+		var point_on_asteroid = asteroid.to_global(collision_point)
+		var player_dir = global_position.direction_to(point_on_asteroid)
+		var player_radius = get_node("player").get_rect().size.x/2
+		set_global_position(point_on_asteroid + player_dir * player_radius)
 
 func _on_asteroid_entered(body: Node) -> void:
-	grapple_collided = false
-	print ("Asteroid collission detected. ")
-	pass # Replace with function body.
+#	if grapple_colided:
+		grapple_collided = false
+		stuck = true
+		asteroid = body
+		print ("Asteroid collission detected. ")
+#	pass # Replace with function body.

c13f9bc43181bab3a155f83418259e55b8efc043
Nicholas Chite <nickman736@gmail.com>
2020-08-01 19:16:21 -0400

    Code start/restart button graphics

--- Src/TextureButton.gd
+++ Src/TextureButton.gd
@@ -5,6 +5,10 @@ extends TextureButton
 # var a = 2
 # var b = "text"
 
+var start_texture: Texture = preload("res://Assets/start.png")
+var restart_texture: Texture = preload("res://Assets/restart.png")
+
+
 var started = false
 
 
@@ -19,13 +23,14 @@ func _ready():
 #	pass
 
 
+
 func _on_StartButton_button_up():
 	
 	if !started:
 		get_tree().paused = false
 		self.visible = false
 		self.disabled = true
-		self.get_node("Label").text = "Exit?"
+		self.texture_normal = restart_texture
 	else:
 		print(get_tree().reload_current_scene())
 		
